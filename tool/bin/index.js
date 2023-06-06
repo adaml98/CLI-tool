401eebcab26df3b2ff384b40f1030729ba690ba6
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-06-06 15:05:56 +0100

    create tools error handling

---
+++ tool/bin/index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const arg = require("arg");
+try {
+  const args = arg({
+    "--start": Boolean,
+    "--build": Boolean,
+  });
+
+  if (args["--start"]) {
+    console.log("starting the app");
+  }
+} catch (e) {
+  console.log(e.message);
+  usage();
+}
+
+function usage() {
+  console.log(`tool [CMD]
+    --start\tStarts the app
+    --build\tBuilds the app`);
+}
